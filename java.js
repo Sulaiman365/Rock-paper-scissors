9ef6df5a85fa2fa32760be3629bbd9c5be5a39ed
Sulaiman Ahmad <sulaiman365@gmail.com>
2022-07-27 13:57:37 +0300

    index.html

--- java.js
+++ java.js
@@ -14,58 +14,74 @@ function getComputerChoice(computerChoice){
 }
 function playRound (playerSelection,computerSelection)
 {
-//const computerSelection = getComputerChoice ();
-//const playerSelection = prompt("Please enter your choice.");
 if (computerSelection==='Scissors' && playerSelection.toUpperCase() === 'ROCK')
 {
 playerScore ++;
 console.log("You Win! Rock beats Scissors");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Win! Rock beats Scissors");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Scissors' && playerSelection.toUpperCase() === 'PAPER')
 {
 computerScore ++;
 console.log("You Lose! Scissors beats Paper");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Lose! Scissors beats Paper");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Scissors' && playerSelection.toUpperCase() === 'SCISSORS')
 {
 console.log("Draw!");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("Draw!");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Paper' && playerSelection.toUpperCase() === 'PAPER')
 {
 console.log("Draw!");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("Draw!");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Paper' && playerSelection.toUpperCase() === 'SCISSORS')
 {
 playerScore++;
 console.log("You Win!  Scissors beats  Paper");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Win!  Scissors beats  Paper");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Paper' && playerSelection.toUpperCase() === 'ROCK')
 {
 computerScore++;
 console.log("You Lose!  Paper beats  Rock");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Lose!  Paper beats  Rock");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Rock' && playerSelection.toUpperCase() === 'ROCK')
 {
 console.log("Draw!");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("Draw!");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Rock' && playerSelection.toUpperCase() === 'SCISSORS')
 {
 computerScore++;
 console.log("You Lose! Rock beats Scissors");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Lose! Rock beats Scissors");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 else if (computerSelection==='Rock' && playerSelection.toUpperCase() === 'PAPER')
 {
 playerScore++;
 console.log("You Win! Paper beats Rock");
 console.log("You:"+playerScore+" : "+computerScore+":Computer");
+alert("You Win! Paper beats Rock");
+alert("You:"+playerScore+" : "+computerScore+":Computer");
 }
 
 }
@@ -81,6 +97,7 @@ playRound(playerSelection,computerSelection);
 else
 {
     console.log("Please enter Rock, Paper or Scissors only.");
+    alert("Please enter Rock, Paper or Scissors only.");
     i--;
 }
 }
@@ -89,11 +106,14 @@ game();
 if (playerScore > computerScore)
 {
 console.log("Humanity has been saved");
+alert("Humanity has been saved");
 }
 else if (playerScore == computerScore)
 {
-    console.log("Draw! You live to fight another day!")
+    console.log("Draw! You live to fight another day!");
+    alert("Draw! You live to fight another day!");
 }
 else{
-    console.log("Machines have colonized the world!")
+    console.log("Machines have colonized the world!");
+    alert("Machines have colonized the world!");
 }
